289a89bd6adec149c8b899a80ab02a24ffb7fef3
Vlad Romanciuc <vladislavromanciuc@gmail.com>
2022-06-22 15:09:24 +0200

    to main

--- week1/Rust/mario.rs
+++ week1/Rust/mario.rs
@@ -18,42 +18,61 @@
 //  ####### #######
 // ######## ########
 
-fn marion(e: u32) {
-    for i in 1..e + 1 {
-        let space = e - i;
-        for _ in (0..space).rev() {
-            print!(" ");
-        }
-        for _ in 0..i {
-            print!("#");
-        }
-        print!(" ");
-        for _ in 0..i {
-            print!("#");
-        }
-        print!("\n");
+
+//main function
+fn main() {
+
+
+println!("Enter a number between 1 and 8: ");
+
+//variable to store the user input
+let input = input();
+
+//condition checker for user input between 1 and 8
+if input > 0 && input <9 {
+    //Call the marion function passing the user input
+    marion(input);
+    }
+else {
+    //if outside of range print info
+    println!("Out of range...");
     }
 }
 
+//function to take the input from user and return the entry as a number
 fn input() -> u32{
+    //variable line of type string
     let mut line  = String::new();
+    //call the terminal reader
     std::io::stdin().read_line(&mut line).unwrap();
+    //trimm the entry for spaces and trnsform to number
     let entry : u32 = line.trim().parse().unwrap();
     return entry;
 }
 
-fn main() {
-    loop {
-        print!("Enter a number between 1 and 8: ");
-
-        let input = input();
-
-        if (input > 0 && input <9) {
-            marion(input);
-            break;
+//The pyramid builder function
+fn marion(e: u32) {
+    //starts from a new line
+    print!("\n");
+    //for loop starts with 1 for math calculations
+    for i in 1..e + 1 {
+        //calculate the empty spaces required
+        let space = e - i;
+        //print empty spaces
+        for _ in (0..space).rev() {
+            print!(" ");
         }
-        else {
-            print!("Out of range...");
+        //print hashes
+        for _ in 0..i {
+            print!("#");
         }
+        //print empty space for second task
+        print!(" ");
+        //print hashes
+        for _ in 0..i {
+            print!("#");
+        }
+        //move to new line
+        print!("\n");
     }
 }
